2b5cfa814403367350bec9d35b2a1bcb97e6ac2a
Antonymaged <magedantony1@gmail.com>
2023-06-09 21:51:13 +0000

    Adding the choice to the user to log in or register

---
+++ Program/system.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	string cho;
+        ch1:
+	cout << "Chose between L/l = login , R/r= register";
+	cin >> cho;
+	if(cho == "L" || cho == "l")
+	{
+		login();
+	}
+	else if(cho == "R" || cho == "r")
+	{
+		usr_register();
+	}
+	else
+	{
+		cout << "Wrong choice try again" << endl;
+		goto ch1;
+	}
+
+	return 0;
+}
