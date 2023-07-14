5cc6fc4bcb08a224917d2a193a9f8bc87a50650b
Antonymaged <magedantony1@gmail.com>
2023-07-14 22:52:37 +0000

    i have add a prototype of the login function and the dunction of the register at which the program take the name, age and number of the user and save it in a file called registers.txt

--- Program/system.cpp
+++ Program/system.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
+
+void usr_register()
+{
+	ofstream file_of_register;
+	file_of_register.open("register.txt");
+	string name,age,number;
+	cout << "please enter your name: ";
+	cin >> name;
+	file_of_register << name <<endl;
+	cout << "please enter your age: ";
+	cin >> age;
+	file_of_register << age  <<endl;
+	cout << "please enter your number: ";
+	cin >> number;
+	file_of_register << number<<endl;
+}
+
+void login()
+{
+cout << "Hi" << endl;
+}
 
 int main()
 {
 	string cho;
         ch1:
-	cout << "Please chose between (L/l) = login , (R/r) = register";
+	cout << "Please chose between (L/l) = login , (R/r) = register\n";
 	cin >> cho;
 	if(cho == "L" || cho == "l")
 	{
